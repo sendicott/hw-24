afce74398a63ac54693f0529bfa6b7fb97fbd25d
Scott Endicott <sendicott1391@gmail.com>
2016-09-22 16:29:07 -0400

    Minor update

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
 let app = angular.module('NewsTrackerApp', []);
 
-app.factory("NewsService", function($http) {
+app.factory("NewsService", function ($http) {
     let newsArray = [];
 
     $http({
         method: 'GET',
         url: "http://puzzlegram.herokuapp.com/news",
-    }).then(function(news) {
-        console.log(news);
+    }).then(function (news) {
+        // console.log(news);
         angular.copy(news.data.news, newsArray);
     });
 
+    for (let i = 0; i < newsArray.length; i++) {
+        newsArray[i].starred = false;
+        console.log(newsArray);
+    }
+
+    // newsArray.forEach( function (arrayItem) {
+    //     arrayItem.starred = false;
+    // });
+
     return {
-        getNews: function() {
+        getNews: function () {
             return newsArray;
         },
 
-        // star: function(selectedArticle) {
+        // star: function (selectedArticle) {
         //     for (let i = 0; i < newsArray.length; i++) {
         //         if (selectedArticle === newsArray[i]) {
 
         //         }
         //     }
         // }
+
     }
-    
 });
 
-app.controller("NewsController", function($scope, NewsService) {
+app.controller("NewsController", function ($scope, NewsService) {
     $scope.news = NewsService.getNews();
-    $scope.StarTheArticle = function(selectedArticle) {
+    console.log(NewsService.getNews());
+    $scope.StarTheArticle = function (selectedArticle) {
         // let starred = angular.element(document.querySelector(''))
         // NewsService.star(selectedArticle);
     }
